e072af526e265ee9ee115128ee9189e65d37e689
gomezjesse27 <116235937+gomezjesse27@users.noreply.github.com>
2023-05-25 12:39:36 -0700

    Before charts Changes

--- Study Farm/AuthViewModel.swift
+++ Study Farm/AuthViewModel.swift
@@ -9,23 +9,6 @@ import Foundation
 import FirebaseAuth
 import FirebaseFirestore
 
-enum TimeRange: String, CaseIterable {
-    case today
-    case week
-    case month
-    case year
-}
-
-struct StudySession {
-    let subject: String
-    let duration: Double
-    let endTime: Date
-}
-struct ChartDataPoint: Identifiable {
-    let id = UUID()
-    let value: Double
-    let label: String
-}
 
 class AuthViewModel: ObservableObject {
     @Published var isSignedIn = false
@@ -47,7 +30,7 @@ class AuthViewModel: ObservableObject {
                 print("User is signed out.")
             }
         }
-        fetchSubjects()
+        
     }
     
     deinit {
@@ -206,167 +189,7 @@ class AuthViewModel: ObservableObject {
     }
     
     
-    
-    // Updated methods to handle local subject storage
-    func getUserSubjects(completion: @escaping ([String]) -> Void) {
-        let subjects = UserDefaults.standard.stringArray(forKey: "subjects") ?? []
-        completion(subjects)
-    }
-    func createSubject(name: String) {
-        var subjects = UserDefaults.standard.stringArray(forKey: "subjects") ?? []
-        subjects.append(name)
-        UserDefaults.standard.setValue(subjects, forKey: "subjects")
-        fetchSubjects()
-    }
-    
-    func deleteSubject(name: String) {
-        var subjects = UserDefaults.standard.stringArray(forKey: "subjects") ?? []
-        subjects.removeAll(where: { $0 == name })
-        UserDefaults.standard.setValue(subjects, forKey: "subjects")
-        fetchSubjects()
-    }
-    
-    func fetchSubjects() {
-        getUserSubjects { (retrievedSubjects) in
-            DispatchQueue.main.async {
-                self.subjects = retrievedSubjects
-            }
-        }
-    }
-        func saveStudySession(subject: String, duration: Double) {
-            guard let user = Auth.auth().currentUser else {
-                return
-            }
-            
-            let studySession: [String: Any] = [
-                "subject": subject,
-                "duration": duration,
-                "endTime": Date().timeIntervalSince1970  // UNIX timestamp
-            ]
-            
-            db.collection("users").document(user.uid).collection("studySessions").addDocument(data: studySession) { error in
-                if let error = error {
-                    print("Error writing study session: \(error)")
-                }
-            }
-        }
-        func getStudySessions(completion: @escaping ([StudySession]) -> Void) {
-            guard let user = Auth.auth().currentUser else {
-                return
-            }
-            
-            db.collection("users").document(user.uid).collection("studySessions").order(by: "endTime").getDocuments { querySnapshot, error in
-                if let error = error {
-                    print("Error reading study sessions: \(error)")
-                } else {
-                    var studySessions: [StudySession] = []
-                    
-                    for document in querySnapshot!.documents {
-                        let data = document.data()
-                        
-                        if let subject = data["subject"] as? String,
-                           let duration = data["duration"] as? Double,
-                           let endTime = data["endTime"] as? TimeInterval {
-                            let studySession = StudySession(subject: subject, duration: duration, endTime: Date(timeIntervalSince1970: endTime))
-                            studySessions.append(studySession)
-                        }
-                    }
-                    
-                    completion(studySessions)
-                }
-            }
-        }
-        func getStudySessionData(timeRange: TimeRange, completion: @escaping ([ChartDataPoint]) -> Void) {
-            guard let user = Auth.auth().currentUser else {
-                return
-            }
-            
-            let (start, end) = timeRangeBoundaries(timeRange)
-            
-            db.collection("users").document(user.uid).collection("studySessions")
-                .whereField("endTime", isGreaterThanOrEqualTo: start)
-                .whereField("endTime", isLessThanOrEqualTo: end)
-                .order(by: "endTime")
-                .getDocuments { querySnapshot, error in
-                    if let error = error {
-                        print("Error reading study sessions: \(error)")
-                    } else {
-                        var chartDataPoints: [ChartDataPoint] = []
-                        
-                        for document in querySnapshot!.documents {
-                            let data = document.data()
-                            
-                            if let subject = data["subject"] as? String,
-                               let duration = data["duration"] as? Double,
-                               let endTime = data["endTime"] as? TimeInterval {
-                                let date = Date(timeIntervalSince1970: endTime)
-                                let dateFormatter = DateFormatter()
-                                dateFormatter.dateFormat = "yyyy-MM-dd"
-                                let dateString = dateFormatter.string(from: date)
-                                let dataPoint = ChartDataPoint(value: duration, label: dateString)
-                                chartDataPoints.append(dataPoint)
-                            }
-                        }
-                        
-                        completion(chartDataPoints)
-                    }
-                }
-        }
         
-    func getSubjectData(timeRange: TimeRange, completion: @escaping ([ChartDataPoint]) -> Void) {
-        guard let user = Auth.auth().currentUser else {
-            return
-        }
-        
-        let (start, end) = timeRangeBoundaries(timeRange)
-        
-        db.collection("users").document(user.uid).collection("studySessions")
-            .whereField("endTime", isGreaterThanOrEqualTo: start)
-            .whereField("endTime", isLessThanOrEqualTo: end)
-            .getDocuments { (querySnapshot, error) in
-                if let error = error {
-                    print("Error getting study sessions: \(error)")
-                } else {
-                    var subjectDurationDict: [String: Double] = [:]
-                    for document in querySnapshot!.documents {
-                        let data = document.data()
-                        if let subject = data["subject"] as? String,
-                           let duration = data["duration"] as? Double {
-                            subjectDurationDict[subject, default: 0] += duration
-                        }
-                    }
-                    
-                    var chartDataPoints: [ChartDataPoint] = []
-                    for (subject, duration) in subjectDurationDict {
-                        let dataPoint = ChartDataPoint(value: duration, label: subject)
-                        chartDataPoints.append(dataPoint)
-                    }
-                    completion(chartDataPoints)
-                }
-            }
-        
-    }
-        
-        func timeRangeBoundaries(_ range: TimeRange) -> (TimeInterval, TimeInterval) {
-            let now = Date()
-            var components: DateComponents?
-            
-            switch range {
-            case .today:
-                components = DateComponents(day: -1)
-            case .week:
-                components = DateComponents(weekOfYear: -1)
-            case .month:
-                components = DateComponents(month: -1)
-            case .year:
-                components = DateComponents(year: -1)
-            }
-            
-            let start = Calendar.current.date(byAdding: components!, to: now)!.timeIntervalSince1970
-            let end = now.timeIntervalSince1970
-            
-            return (start, end)
-        }
         
     }
     
--- Study Farm/ProfileView.swift
+++ Study Farm/ProfileView.swift
@@ -10,49 +10,13 @@ import SwiftUICharts
 
 struct ProfileView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
-    @State private var studySessionData: [ChartDataPoint] = []
-    @State private var subjectData: [ChartDataPoint] = []
-    @State private var selectedTimeRange = TimeRange.today
-    let timeRanges = TimeRange.allCases
     
     let colors: [Color] = [.red, .blue, .green, .orange, .purple, .pink]  // Add more if needed
-
+    
     var body: some View {
         ScrollView {
             VStack {
-                Text("Select Time Range")
-                    .font(.title2)
-                Picker("Select Time Range", selection: $selectedTimeRange) {
-                    ForEach(timeRanges, id: \.self) {
-                        Text($0.rawValue.capitalized)
-                    }
-                }
-                .pickerStyle(.segmented)
-                .padding()
-
-                Text("Study Session Duration")
-                    .font(.title2)
-                LineChartView(data: studySessionData.map { $0.value }, title: "Study Session Duration", legend: "Daily Duration")
-                    .padding()
-
-                Text("Subject Distribution")
-                    .font(.title2)
-                PieChartView(data: subjectData.map { $0.value }, title: "Subject Distribution", legend: "Subject Hours")
-                    .padding()
-
-                // Legend
-                VStack(alignment: .leading) {
-                    ForEach(0..<subjectData.count) { index in
-                        HStack {
-                            Circle()
-                                .fill(colors[index % colors.count])
-                                .frame(width: 10, height: 10)
-                            Text(subjectData[index].label)
-                        }
-                    }
-                }
-                .padding()
-
+                
                 // Your existing sign-out button code...
                 Button(action: {
                     authViewModel.signOut()
@@ -65,22 +29,11 @@ struct ProfileView: View {
                         .cornerRadius(15)
                 }
             }
-            .onAppear {
-                loadStudyData()
-            }
-            .onChange(of: selectedTimeRange) { newValue in
-                loadStudyData()
-            }
+            
         }
+        
     }
-
-    private func loadStudyData() {
-        authViewModel.getStudySessionData(timeRange: selectedTimeRange) { data in
-            studySessionData = data
-        }
-        authViewModel.getSubjectData(timeRange: selectedTimeRange) { data in
-            subjectData = data
-        }
-    }
+    
+    
 }
 
